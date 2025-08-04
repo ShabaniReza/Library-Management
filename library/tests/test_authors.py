b7810533d9989b3e136455bfbb18c9bad4b8e99a
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-08-04 18:58:45 +0330

    creating two more fixtures

--- library/tests/test_authors.py
+++ library/tests/test_authors.py
@@ -2,10 +2,17 @@ from rest_framework import status
 from rest_framework.test import APIClient
 import pytest
 
+
+@pytest.fixture
+def create_author(api_client):
+    def do_create_author(author_information):
+        return api_client.post('/library/authors/', author_information)
+    return do_create_author
+
+
 @pytest.mark.django_db
 class TestCreateAuthor:
-    def test_if_user_is_anonymous_returns_401(self, api_client):
-        client = APIClient()
-        response = api_client.post('/library/authors/', {'first_name': 'a'})
+    def test_if_user_is_anonymous_returns_401(self, create_author):
+        response = create_author({'first_name': 'a'})
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
