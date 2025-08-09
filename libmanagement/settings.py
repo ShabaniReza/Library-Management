a647cdb92ea6c93dd7d2f8dd58e003e13f5f4246
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-08-09 16:29:32 +0330

    installing django-redis and settings configuration

--- libmanagement/settings.py
+++ libmanagement/settings.py
@@ -192,3 +192,14 @@ DJANGO_REST_PASSWORDRESET_TOKEN_CONFIG = {
 
 
 CELERY_BROKER_URL = 'redis://redis:6379/1'
+
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/2",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
