11f0e106eb1f6c5d46f2e7ce6cc046e2f9e6f4f5
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-08-23 16:35:43 +0330

    creating two services in compose file and adding depends_on attribute to services that need

--- libmanagement/settings.py
+++ libmanagement/settings.py
@@ -198,7 +198,7 @@ CELERY_BROKER_URL = 'redis://redis:6379/1'
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/2",
+        "LOCATION": "redis://redis:6379/2",
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
         }
