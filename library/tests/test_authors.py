4553c94b57966eae4bc07dffbddae330331b32dc
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-08-27 10:16:20 +0330

    fixing an issue in tests

--- library/tests/test_authors.py
+++ library/tests/test_authors.py
@@ -14,14 +14,14 @@ def create_author(api_client):
 @pytest.mark.django_db
 class TestCreateAuthor:
     def test_if_user_is_anonymous_returns_401(self, create_author):
-        response = create_author({'first_name': 'a'})
+        response = create_author({'first_name': 'a', 'last_name': 'b'})
 
         assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
     def test_if_user_is_not_admin_returns_403(self, authenticate, create_author):
         authenticate()
 
-        response = create_author({'first_name': 'a'})
+        response = create_author({'first_name': 'a', 'last_name': 'b'})
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
 
