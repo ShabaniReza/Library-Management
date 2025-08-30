9a5205f5d0247b4cbac6b890b06991732a024e0d
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-08-30 15:51:14 +0330

    creating a new test for status that admin patch author with invalid data

--- library/tests/test_authors.py
+++ library/tests/test_authors.py
@@ -105,3 +105,12 @@ class TestPatchAuthor:
         response = patch_author({'first_name': 'a'}, author.id)
 
         assert response.status_code == status.HTTP_200_OK
+
+    def test_if_data_is_invalid_returns_400(self, authenticate, patch_author):
+        author = baker.make(Author)
+        authenticate(True)
+
+        response = patch_author({'first_name': ''}, author.id)
+
+        assert response.status_code == status.HTTP_400_BAD_REQUEST
+        assert response.data['first_name'] is not None
