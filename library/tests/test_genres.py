9e8551d41c589353021e71960c3a77682f7c463e
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-08-31 17:16:17 +0330

    creating a fixture for creating a genre

--- library/tests/test_genres.py
+++ library/tests/test_genres.py
@@ -2,3 +2,12 @@ from rest_framework import status
 from model_bakery import baker
 from library.models import Genre
 import pytest
+
+
+# ! ____________________________Fixtures____________________________
+
+@pytest.fixture
+def create_genre(api_client):
+    def do_create_genre(genre_information):
+        return api_client.post('/library/genres/', genre_information)
+    return do_create_genre
