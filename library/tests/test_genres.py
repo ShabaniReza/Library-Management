c4101ab08213a507ac4d264792422eaecc747fa4
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-09-01 13:46:00 +0330

    creating a new test for anonymous user send post request to genres endpoint

--- library/tests/test_genres.py
+++ library/tests/test_genres.py
@@ -11,3 +11,12 @@ def create_genre(api_client):
     def do_create_genre(genre_information):
         return api_client.post('/library/genres/', genre_information)
     return do_create_genre
+
+# ! ____________________________Create____________________________
+
+@pytest.mark.django_db
+class TestCreateGenre:
+    def test_if_user_is_anonymous_returns_401(self, create_genre):
+        response = create_genre({'name': 'a'})
+
+        assert response.status_code == status.HTTP_401_UNAUTHORIZED
