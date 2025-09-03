1742cb232c3720e49e905cdaa436f0127a2efc18
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-09-03 16:57:52 +0330

    creating a new task for performance testing of authors endpoint

--- locustfiles/locust_authors.py
+++ locustfiles/locust_authors.py
@@ -1,9 +1,14 @@
 from locust import HttpUser, task, between
+from random import randint
 
 class WebsiteUser(HttpUser):
     wait_time = between(1, 5)
 
-    @task()
+    @task(2)
     def view_authors(self):
         self.client.get('/library/authors/', name='View authors')
         
+    @task(1)
+    def view_author(self):
+        author_id = randint(1, 20)
+        self.client.get(f'/library/authors/{author_id}', name='View author')
