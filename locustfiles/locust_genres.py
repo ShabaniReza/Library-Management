e848c3d146b685bce086dcb9d8fa79b43cf3b066
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-09-06 10:34:11 +0330

    creating a new performance test for testing genres endpoint in getting an specific genre

--- locustfiles/locust_genres.py
+++ locustfiles/locust_genres.py
@@ -1,8 +1,14 @@
 from locust import HttpUser, task, between
+from random import randint
 
 class GenresEndpoint(HttpUser):
     wait_time = between(1, 5)
 
-    @task()
+    @task(2)
     def view_genres(self):
         self.client.get('/library/genres/', name='View genres')
+
+    @task(1)
+    def view_genre(self):
+        genre_id = randint(1, 20)
+        self.client.get(f'/library/genres/{genre_id}/', name='View a genre')
