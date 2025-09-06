ac3170bdebf8b08fd76dc93a469113b02e81f174
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-09-06 13:50:43 +0330

    creating a new task for testing performance of books endpoint in getting a single item

--- locustfiles/locust_books.py
+++ locustfiles/locust_books.py
@@ -1,7 +1,14 @@
 from locust import HttpUser, task, between
+from random import randint
 
 class BooksEndpoint(HttpUser):
     wait_time = between(1, 5)
 
+    task(2)
     def view_books(self):
         self.client.get('/library/books/', name='View books')
+
+    task(1)
+    def view_book(self):
+        book_id = randint(1, 20)
+        self.client.get(f'/library/books/{book_id}/', name='View a book')
