2e5e974fa85002b4f51d9498b43fd25c92fbab8e
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-11-10 16:44:16 +0330

    delete some unnecessary line in prod file

--- libmanagement/settings/prod.py
+++ libmanagement/settings/prod.py
@@ -1,8 +1,5 @@
-from dotenv import load_dotenv
-import os
 from .common import *
 
-load_dotenv()
 
 DEBUG = False
 
@@ -11,13 +8,6 @@ ALLOWED_HOSTS = ['reza27.pythonanywhere.com']
 REDIS_URL = os.getenv('REDIS_URL')
 
 CELERY_BROKER_URL = REDIS_URL
-
-REDIS_OPTIONS = {}
-if REDIS_URL and REDIS_URL.startswith('rediss://'):
-    REDIS_OPTIONS = {
-        "SSL_CERT_REQS": None, 
-        "SSL_CERTFILE": None, 
-    }
 
 CACHES = {
     "default": {
