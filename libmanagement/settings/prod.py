a119ee4b20ff8822ea172101fdccb1e1fdc8ac26
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-11-10 09:44:48 +0330

    preparing to use ssl connection to connect redis host

--- libmanagement/settings/prod.py
+++ libmanagement/settings/prod.py
@@ -12,6 +12,12 @@ REDIS_URL = os.getenv('REDIS_URL')
 
 CELERY_BROKER_URL = REDIS_URL
 
+REDIS_OPTIONS = {}
+if REDIS_URL and REDIS_URL.startswith('rediss://'):
+    REDIS_OPTIONS = {
+        "SSL_CERT_REQS": None, 
+        "SSL_CERTFILE": None, 
+    }
 
 CACHES = {
     "default": {
@@ -19,6 +25,7 @@ CACHES = {
         "LOCATION": REDIS_URL,
         "OPTIONS": {
             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {"ssl_cert_reqs": None},
         }
     }
 }
