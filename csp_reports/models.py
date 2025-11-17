5601560295c0c67568e81f3c22ad224ad2e62f4a
Reza Shabani <rezashabani.27.1379@gmail.com>
2025-11-17 14:47:27 +0330

    creating a model for save csp reports data

--- csp_reports/models.py
+++ csp_reports/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+class CSPViolationReport(models.Model):
+    blocked_uri = models.CharField(max_length=512, blank=True, null=True)
+    document_uri = models.CharField(max_length=512)
+    violated_directive = models.CharField(max_length=256)
+    report_time = models.DateTimeField(auto_now_add=True)
+    full_report = models.JSONField() 
+
+    class Meta:
+        ordering = ['-report_time']
+
+    def __str__(self):
+        return f"تخطی {self.violated_directive} در {self.document_uri}"
